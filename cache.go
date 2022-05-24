5d0911e0fefdfb58953e3d67c51cc67649d1fa38
Artyom Belski <artbelski@gmail.com>
2022-05-24 22:50:51 +0300

    First try

--- cache.go
+++ cache.go
@@ -1,23 +1,46 @@
 package cache
 
-import "time"
+import (
+	"time"
+)
+
+type containment struct {
+	value string
+	temporary bool
+	before time.Time
+}
 
 type Cache struct {
+	container map[string]containment
 }
 
 func NewCache() Cache {
-	return Cache{}
+	return Cache{map[string]containment{}}
 }
 
-func (receiver) Get(key string) (string, bool) {
-
+func (c Cache) Get(key string) (string, bool) {
+	if val, ok := c.container[key]; ok {
+		if !val.temporary || time.Now().Before(val.before) {
+			return val.value, true
+		}
+	}
+	return "", false
 }
 
-func (receiver) Put(key, value string) {
+func (c Cache) Put(key, value string) {
+	c.container[key] = containment{value, false, time.Time{}}
 }
 
-func (receiver) Keys() []string {
+func (c Cache) Keys() []string {
+	keys := make([]string, len(c.container))
+	counter := 0
+	for key := range c.container {
+		keys[counter] = key
+		counter++
+	}
+	return keys
 }
 
-func (receiver) PutTill(key, value string, deadline time.Time) {
+func (c Cache) PutTill(key, value string, deadline time.Time) {
+	c.container[key] = containment{value, true, deadline}
 }
